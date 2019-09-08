c3288648a90542bd4d7dc86cedfc643c5472932d
Nodir Siddikov <nodirsid@gmail.com>
2019-09-08 21:31:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,5 +27,10 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+  switch(age){
+    case 12, 13, 14, 15, 16, 17, 18, 19:
+      return "You are a teenager";
+    default:
+      return "You have an age";
+  }
 }
